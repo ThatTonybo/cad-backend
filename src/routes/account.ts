1de293abda37d17bd849a20ceba643993190fe50
Ally Summers <ally.summers@outlook.com.au>
2022-08-19 20:32:20 +1000

    Fixing issue with mongoose schemas not being registered, plus added ts-node and nodemon to easily develop locally

--- src/routes/account.ts
+++ src/routes/account.ts
@@ -1,18 +1,16 @@
 import { Router, Request, Response } from 'express';
-import mongoose from 'mongoose';
+import { Account } from '../models/Account';
 
 export const route = Router();
 
-const Account = mongoose.model('Account');
-
 route.get('/create', async (req: Request, res: Response) => {
-    const account = new Account({
-        email: 'test'
-    });
+  const account = new Account({
+    email: 'test',
+  });
 
-    await account.save();
+  await account.save();
 
-    return res.json({
-        success: true
-    });
+  return res.json({
+    success: true,
+  });
 });
